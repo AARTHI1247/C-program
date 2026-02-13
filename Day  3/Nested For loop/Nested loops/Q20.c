86347da4e647210ace4e740f5323fb1265ee0737
AARTHI.M <88aarthiaarthi70@gmail.com>
2026-02-13 00:49:58 -0800

    Refactor nested loops for pattern printing

--- Day  3/Nested For loop/Nested loops/Q20.c
+++ Day  3/Nested For loop/Nested loops/Q20.c
@@ -1,17 +1,33 @@
-#include<stdio.h>
-int main(){
-    int n;
-    scanf("%d",&n);
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=i;j++){
-            printf("*");
-        }
-        for(int j=1;j<=((2*n)-(2*i));j++){
-            printf(" ");
-        }
-       for(int j=i;j>=1;j--){
-        printf("*");
+#include <stdio.h>
+int main() {
+   int n;
+   scanf("%d",&n);
+   for (int i=0;i<n;i++){
+    
+       for(int j=0;j<i+1;j++){
+           printf("*");
+       }
+       for(int j=(2*n)-2;j>i*2;j--){
+           printf(" ");
+       }
+
+       for(int j=0;j<i+1;j++){
+           printf("*");
        }
        printf("\n");
-    }
+   }
+   for(int i=0;i<n;i++){
+       for(int j=n-1;j>i;j--){
+           printf("*");
+       }
+       for(int j=0;j<(i*2)+2;j++){
+           printf(" ");
+       }
+      for(int j=n-1;j>i;j--){
+           printf("*");
+       }
+       
+       printf("\n");
+   }
+    return 0;
 }

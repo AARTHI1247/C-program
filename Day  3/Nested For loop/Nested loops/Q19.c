3632f6385e7c5adc59a663d7e4f770b7d271e2e4
AARTHI.M <88aarthiaarthi70@gmail.com>
2026-02-13 00:45:58 -0800

    Refactor nested loops for better star pattern output

--- Day  3/Nested For loop/Nested loops/Q19.c
+++ Day  3/Nested For loop/Nested loops/Q19.c
@@ -1,32 +1,19 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main() {
-   int n;
-   scanf("%d",&n);
-   for (int i=0;i<n;i++){
-       for(int j=0;j<i+1;j++){
-           printf("*");
-       }
-       for(int j=(2*n)-2;j>i*2;j--){
-           printf(" ");
-       }
-       for(int j=0;j<i+1;j++){
-           printf("*");
-       }
-       printf("\n");
-   }
-   for(int i=0;i<n;i++){
-       for(int j=n-1;j>i;j--){
-           printf("*");
-       }
-       for(int j=0;j<(i*2)+2;j++){
-           printf(" ");
-       }
-      for(int j=n-1;j>i;j--){
-           printf("*");
-       }
-       
-       printf("\n");
-   }
-    return 0;
+int main(){
+    int n;
+    scanf("%d",&n);
+
+    for(int i=0;i<n;i++){
+        for(int j=1;j<=n-i;j++){
+            printf("*");
+        } 
+        for (int j=1;j<=2*i;j++){
+            printf(" ");
+        }
+        for(int j=1;j<=n-i;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
 }

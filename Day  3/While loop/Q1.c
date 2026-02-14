825538cd9091ee8867603d69ab8fd661b62a54f1
AARTHI.M <88aarthiaarthi70@gmail.com>
2026-02-13 17:22:25 -0800

    Update score counting logic in Q1.c
    Refactor input handling and counting logic for scores.

--- Day  3/While loop/Q1.c
+++ Day  3/While loop/Q1.c
@@ -2,15 +2,23 @@
 int main(){
     int n,c=0;
     scanf("%d",&n);
-    int a[n];
-   for(int i=0;i<n;i++){
-        scanf("%d",&a[i]);
-    }
-        int i=0;
-        while(a[i]>=70){
+    int i=0;
+    int c1=0,c2=0;
+    int a;
+    while(i<n){
+        scanf("%d",&a);
+        if(a>=70){
             c++;
-            i++;
+            c1++;
+        
+            if(c1>c2){
+              c2=c1;
         }
-        printf("%d",c);
     }
-    
+        else{
+            c1=0;
+        }
+        i++;
+    }
+    return 0;
+}

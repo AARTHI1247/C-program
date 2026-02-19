f54856da773827c7c7c4b8dca17a295caa84011c
AARTHI.M <88aarthiaarthi70@gmail.com>
2026-02-18 20:35:24 -0800

    Create Q2.c

---
+++ Strings/Q2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char arr[20];
+    fgets(arr,sizeof(arr),stdin);
+    int count=0;
+    for(int i=0;i<arr[i]!=0;i++){
+        if(arr[i]>='A' && arr[i]<='Z'){
+            count++;
+           
+        }}
+        if(count>0){
+             printf("Valid");
+        }
+        else{
+            printf("Invalid");
+        }
+ }
+   
+    

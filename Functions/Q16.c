6da1448194d5abcba2d0eaf0862dc9516ea58fa1
AARTHI.M <88aarthiaarthi70@gmail.com>
2026-02-22 01:39:29 -0800

    Add Fibonacci sequence function and main program

---
+++ Functions/Q16.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<math.h>
+void fibonacci(int n){
+    int a=0,b=1,c;
+    printf("%d %d",a,b);
+    for(int i=2;i<n;i++){
+        c=a+b;
+        printf(" %d",c);
+        a=b;
+        b=c;
+    }
+}
+int main(){
+    int n;
+    scanf("%d",&n);
+    fibonacci(n);
+    return 0;
+}

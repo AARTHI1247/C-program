935398bf5e3203222d16cde63a747c07bb02d910
AARTHI.M <88aarthiaarthi70@gmail.com>
2026-02-25 08:47:57 -0800

    Fix code and implement pointer swap functionality

--- Pointers/Q1.c
+++ Pointers/Q1.c
@@ -1,4 +1,12 @@
-wertyinclude
-pointe
-wertfws
-fghhgfdfg
+#include<stdio.h>
+int main(){
+    int a,b;
+    scanf("%d %d",&a,&b);
+    int *p1=&a;
+    int *p2=&b;
+    int *temp;
+    temp=p1;
+    p1=p2;
+    p2=temp;
+    printf("Before swap: %d %d After swap:%d %d",a,b,*p1,*p2);
+}

6ff33e3fb04a0ac4852dce10b20ae452045491a2
AARTHI.M <88aarthiaarthi70@gmail.com>
2026-02-26 08:19:22 -0800

    Add program to find maximum in user-input array
    Implement a program to find the maximum value in an array.

---
+++ Pointers/Q2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int a;
+    scanf("%d",&a);
+    int arr[a];
+    for(int i=0;i<a;i++){
+        scanf("%d",&arr[i]);
+    }
+    int *p=&arr[0];
+    int max=*p;
+    for(int i=1;i<a;i++){
+        if(max<*(p+i)){
+            max=*(p+i);
+        }
+    }
+    printf("%d",max);
+}

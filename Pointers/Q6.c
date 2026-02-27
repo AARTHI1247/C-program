fd15c67c1c6b9fc3eb01d07fd57a5621cc650f0a
AARTHI.M <88aarthiaarthi70@gmail.com>
2026-02-26 17:31:50 -0800

    Add array reversal program in Q6.c
    Implement a program to read an array and reverse its elements.

---
+++ Pointers/Q6.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    int a;
+    scanf("%d\n",&a);
+    int arr[a];
+    for(int i=0;i<a;i++){
+        scanf("%d",&arr[i]);
+    }
+    int a2[a];
+    int *p=arr;
+    for(int i=0;i<a;i++){
+       a2[i]=*(p+(a-1)-i);
+    }
+    for(int i=0;i<a;i++){
+        printf("%d ",a2[i]);
+    }
+}

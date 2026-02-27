2607573ff7ed0130d9767aa1485b1eace082b3a3
AARTHI.M <88aarthiaarthi70@gmail.com>
2026-02-26 17:49:56 -0800

    Add palindrome check in Q7.c
    Implement palindrome check using pointers

---
+++ Pointers/Q7.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char arr[100];
+    fgets(arr,sizeof(arr),stdin);
+    arr[strcspn(arr,"\n")]='\0';
+    char a2[100];
+    char *p=arr;
+    for(int i=0;i<strlen(arr);i++){
+        a2[i]=*(p+(strlen(arr)-1)-i);
+    }
+    a2[strlen(arr)] = '\0';
+    if(strcmp(arr,a2)==0){
+        printf("Yes");
+    } 
+    else{
+        printf("No");
+    } 
+    }
+    

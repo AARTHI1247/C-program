295cc8ce1f32006ac4f130149198db786c4adb94
AARTHI.M <88aarthiaarthi70@gmail.com>
2026-02-27 20:19:45 -0800

    Reverse array elements in place
    Reversed the elements of the array in place and removed unused variables.

--- Pointers/Q6.c
+++ Pointers/Q6.c
@@ -1,5 +1,4 @@
 #include<stdio.h>
-#include<string.h>
 int main(){
     int a;
     scanf("%d\n",&a);
@@ -7,12 +6,15 @@ int main(){
     for(int i=0;i<a;i++){
         scanf("%d",&arr[i]);
     }
-    int a2[a];
-    int *p=arr;
-    for(int i=0;i<a;i++){
-       a2[i]=*(p+(a-1)-i); 
+    int *p=&arr[0];
+    int temp;
+    int *q=&arr[a-1];
+    for(int i=0;i<a/2;i++){
+      temp=*(p+i);
+      *(p+i)=*(q-i);
+      *(q-i)=temp;
     }
     for(int i=0;i<a;i++){
-        printf("%d ",a2[i]);
+        printf("%d ",arr[i]);
     }
 }

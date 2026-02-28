c17830db6be79a91bcd4c6353a35c3457149bfbc
AARTHI.M <88aarthiaarthi70@gmail.com>
2026-02-28 09:20:31 -0800

    Add program to count non-space characters in input

---
+++ Strings Extra/Q3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char arr[50];
+    fgets(arr,50,stdin);
+    arr[strcspn(arr,"\n")]='\0';
+    int count=0;
+    for(int i=0;i<strlen(arr);i++){
+        if(arr[i]!=' '){ 
+            count++;
+        }   
+    } 
+    printf("%d",count);
+}

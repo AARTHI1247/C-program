d6b2087af2120b939936c824c2eb71f4762b37ea
AARTHI.M <88aarthiaarthi70@gmail.com>
2026-02-28 21:56:17 -0800

    remove duplicates in string

---
+++ Strings Extra/Q9.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+#include<stdlib.h>
+int main(){
+    char arr[50];
+    fgets(arr,50,stdin);
+    arr[strcspn(arr,"\n")]='\0';
+    int len=strlen(arr);
+    int c=1,f;
+    for(int i=0;i<len;i++){
+        f=1;
+        for(int j=i+1;j<len;j++){
+           if(arr[i]==arr[j]){
+            arr[j]=0;
+        }     
+    }
+    if(arr[i]!=0){
+        printf("%c",arr[i]);
+    }
+}
+}

6034b51d063aea8a9716b42809277dbcada7df6e
AARTHI.M <88aarthiaarthi70@gmail.com>
2026-03-02 00:42:00 -0800

    Anagram

---
+++ Strings Extra/Part 1/Q13.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+#include<stdlib.h>
+int main(){
+    char arr1[50];
+    fgets(arr1,50,stdin);
+    arr1[strcspn(arr1,"\n")]='\0';
+    char arr2[50];
+    fgets(arr2,50,stdin);
+    arr2[strcspn(arr2,"\n")]='\0';
+    int len1=strlen(arr1);
+    int len2=strlen(arr2);
+    int c=0,f;
+    if(len1==len2){
+    for(int i=0;i<len1;i++){
+        f=0; 
+        for(int j=0;j<len2;j++){
+            if(arr1[i]==arr2[j]){
+                arr2[j]=0;
+                f=1;
+                c++;
+                break;
+            } 
+        }
+        if(f==0){
+            printf("Not anagram");
+            return  0;
+        }
+    }
+           if(c==len1){
+            printf("Anagram");
+           }  
+    }
+else{
+    printf("Not anagram");
+}
+}

2b4da049279c0a3c33106317dda363d5100b2c75
AARTHI.M <88aarthiaarthi70@gmail.com>
2026-03-01 23:53:20 -0800

    Update Q1.c

--- Strings Extra/Part 2/Q1.c
+++ Strings Extra/Part 2/Q1.c
@@ -3,7 +3,7 @@
 int main(){
     char arr[50];
     fgets(arr,50,stdin);
-    arr[strcspn(arr,"\n")]='\0';
+    arr[strcspn(arr,"\n")]='\0';                
     int len=strlen(arr);
     int c=0,f;
     for(int i=0;i<len;i++){

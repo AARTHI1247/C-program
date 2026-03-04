08486f711618c3ebb8751a26d8f2626a6911b2f6
AARTHI.M <88aarthiaarthi70@gmail.com>
2026-03-03 21:13:54 -0800

    Create Q14.c

---
+++ Strings Extra/Part 1/Q14.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+#include<stdlib.h>
+int main(){
+    char arr[50];
+    fgets(arr,50,stdin);
+    arr[strcspn(arr,"\n")]='\0';
+    int len=strlen(arr);
+    char arr2[50];
+    int j=0;
+    for(int i=len-1;i>=0;i--){
+        if(arr[i]!=' '){
+           arr2[j]=arr[i];
+           j++;
+        }
+        else{
+            arr2[j]='\0';
+            for(int i=strlen(arr2)-1;i>=0;i--){
+                 printf("%c",arr2[i]);
+            }
+           j=0;
+           printf(" ");
+        }
+    }
+    if(j>0){
+        arr2[j]='\0';
+        for(int i=strlen(arr2);i>=0;i--){
+            printf("%c",arr2[i]);
+        }
+    }
+}
